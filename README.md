c8d743a0ef68eb090ac445def8dd2cd10e396e76
jupitersh <arxhd.love@qq.com>
2019-12-16 15:42:45 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# Translucent Rabbit House
+
+## Overview
+
+This is a mod for the game of Don't Starve Together which is available in the Steam Workshop. 
+
+The mod makes the rabbit house translucent. which makes it easier to pick meats and corrots in your rabbit house farm.
+
+## Changelog
+
+## License
+
+Released under the [GNU GENERAL PUBLIC LICENSE](https://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+name = "Translucent Rabbit House"
+description = [[
+Make the rabbit house translucent.
+]]
+author = "辣椒小皇纸"
+version = "1.0"
+forumthread = ""
+api_version = 10
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = false
+dst_compatible = true
+client_only_mod = true
+all_clients_require_mod = false
